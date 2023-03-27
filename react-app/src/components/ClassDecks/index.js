bc14c4a7703a6fc6edfad13545d040fb7d958a45
John <tim@cruz-family.ws>
2023-03-26 21:58:23 -0700

    working on decks

--- react-app/src/components/ClassDecks/index.js
+++ react-app/src/components/ClassDecks/index.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import CreateDeckModal from "../CreateDeckModal";
 import OpenModalButton from "../OpenModalButton";
+import PreviewFlashcardsModal from "../PreviewFlashcardsModal";
 import "./ClassDecks.css"
 
 const ClassDecks = ({ props }) => {
@@ -24,11 +25,26 @@ const ClassDecks = ({ props }) => {
             </div>
             {chosenClass?.decks?.length ? chosenClass?.decks.map((deck, idx) => (
                 <div className="deck-container" onClick={() => history.push(`/study/decks/${deck?.id}`)} key={idx}>
-                    <i className="fa-regular fa-circle fa-xl" />
-                    <div className="deck-progress-percent">20%</div>
-                    <div className=" deck-info-container">
-                        <div className="deck-name">{deck.name}</div>
-                        <div className="deck-progress-bar"></div>
+                    <div className="deck-container-left">
+                        <i className="fa-regular fa-circle fa-xl" />
+                        <div className="deck-progress-percent">%</div>
+                        <div className=" deck-info-container">
+                            <div className="deck-name">{deck.name}</div>
+                            <div className="deck-progress-bar"></div>
+                        </div>
+                    </div>
+                    <div className={`deck-container-right ${deck?.flashcards?.length ? "" : "empty"}`}>
+                        <div onClick={(e) => e.stopPropagation()}>
+                            <OpenModalButton
+                                modalComponent={<PreviewFlashcardsModal />}
+                                buttonText={<i className={`fa-solid fa-glasses fa-lg ${chosenClass?.id === session?.user?.id ? deck?.flashcards?.length ? "" : "hidden" : "hidden"}`} />}
+                            />
+                        </div>
+                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-edit fa-solid fa-pencil fa-lg" />
+                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-options fa-solid fa-ellipsis fa-lg" />
+                        <div onClick={(e) => e.stopPropagation()} className="deck-container-add">ADD CARDS</div>
+                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-study fa-solid fa-circle-play fa-lg" />
+                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-view fa-solid fa-angle-right fa-lg" />
                     </div>
                 </div>
             ))
---
+++ react-app/src/components/PreviewFlashcardsModal/index.js
@@ -0,0 +1,12 @@
+import "./PreviewFlashcardsModal.css"
+
+const PreviewFlashcardsModal = () => {
+
+    return (
+        <div>
+            a
+        </div>
+    )
+}
+
+export default PreviewFlashcardsModal
