37781c68c887f4026cac080ef2edd1a700f80d49
John <tim@cruz-family.ws>
2023-03-27 08:43:43 -0700

    trying to fix render deploy

--- migrations/versions/60fd042dc468_.py
+++ migrations/versions/60fd042dc468_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b0ddae07cbb5
-Revises:
-Create Date: 2023-03-22 15:59:36.114222
+Revision ID: 60fd042dc468
+Revises: 
+Create Date: 2023-03-27 08:43:10.585538
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b0ddae07cbb5'
+revision = '60fd042dc468'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -33,7 +33,7 @@ def upgrade():
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('name', sa.String(length=250), nullable=False),
     sa.Column('mix_type', sa.String(), nullable=False),
-    sa.Column('visibility', sa.Boolean(), nullable=False),
+    sa.Column('visibility', sa.String(), nullable=False),
     sa.Column('image', sa.String(), nullable=True),
     sa.Column('headline', sa.String(length=250), nullable=True),
     sa.Column('description', sa.String(length=500), nullable=True),
