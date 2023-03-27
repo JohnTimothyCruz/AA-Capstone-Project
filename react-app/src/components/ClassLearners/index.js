3b3dede0b88b558589d27d8230127dd0e5a64908
John <tim@cruz-family.ws>
2023-03-27 15:01:26 -0700

    add learner-button styled

--- react-app/src/components/ClassLearners/index.js
+++ react-app/src/components/ClassLearners/index.js
@@ -1,9 +1,57 @@
+import OpenModalButton from "../OpenModalButton";
 import "./ClassLearners.css"
 
 const ClassLearners = () => {
 
     return (
-        <div>Heyo learners here.</div>
+        <div id="learners-page-container">
+            <div id="learner-info-bar">
+                <p id="learner-prompt" className="learner-info-bar-header">Learner</p>
+                <p className="learner-info-bar-header">Mastery</p>
+                <p className="learner-info-bar-header">Days Studied</p>
+                <p className="learner-info-bar-header">Time Studied</p>
+                <p className="learner-info-bar-header">Cards Studied</p>
+                <p className="learner-info-bar-header">Cards Permission</p>
+            </div>
+            {/* <div id="selected-learners-options">
+                <div id="selected-learners-options-left">
+                    <p>Bulk actions for 1 selected Learner(s)</p>
+                </div>
+                <div id="selected-learners-options-right">
+                    <i class="fa-solid fa-angle-up" />
+                </div>
+            </div> */}
+            <div id="learners-container">
+
+                <div id="add-learner-container">
+                    <OpenModalButton
+                        buttonText={
+                            <div id="add-learner-container">
+                                <i id="add-learner-circle" className="fa-regular fa-circle fa-xl" />
+                                <i id="add-learner-icon" class="fa-solid fa-user-plus fa-xl" />
+                                <div id="add-learner-right">
+                                    <p id="add-learner-prompt">Add New Learner</p>
+                                    <div id="add-learner-progress-bar"></div>
+                                </div>
+                            </div>
+                        }
+                    />
+                </div>
+            </div>
+            <div id="learners-page-extras">
+                <div id="share-link-container">
+                    <p>SHARE LINK</p>
+                    <div>
+                        <i className="fa-solid fa-link" />
+                        <p>Link goes here</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-clipboard" />
+                        <p>Copy to Clipboard</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
