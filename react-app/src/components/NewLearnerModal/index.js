14f8ba099e0ca66960479bc50875b25a44156c8a
John <tim@cruz-family.ws>
2023-03-27 20:30:07 -0700

    learner page styled

--- react-app/src/components/NewLearnerModal/index.js
+++ react-app/src/components/NewLearnerModal/index.js
@@ -15,13 +15,17 @@ const NewLearnerModal = ({ props }) => {
 
     return (
         <div id="new-learner-modal">
-            <i id="close-new-learner-modal-button" className="fa-solid fa-xmark fa-2xl" onClick={() => closeModal()}/>
+            <i id="close-new-learner-modal-button" className="fa-solid fa-xmark fa-2xl" onClick={() => closeModal()} />
             <h2 id="new-learner-prompt">Share Class By Link</h2>
             <h3 id="new-learner-explaination">The custom link below will give anyone full study access to {<span id="new-learner-class">{chosenClass?.name}</span>}. You can paste it anywhere, including on your website, blog, or in social media.</h3>
             <p id="new-learner-link">(link goes here)</p>
             <div id="new-learner-copy" onClick={() => copyTo()} className={copied ? "copied" : ""}>
-                <p id="new-learner-copy-text">Copy to Clipboard</p>
-                <i class="fa-regular fa-clipboard" />
+                {copied ?
+                    <p className="new-learner-copy-text">Copied to Clipboard</p>
+                    :
+                    <p className="new-learner-copy-text">Copy to Clipboard</p>
+                }
+                <i className="fa-regular fa-clipboard" />
             </div>
             <div id="new-learner-done-button" onClick={() => closeModal()}>DONE</div>
         </div>
