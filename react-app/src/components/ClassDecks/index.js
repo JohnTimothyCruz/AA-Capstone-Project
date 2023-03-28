a9c3c3a5acabc9b858a072026849bb2574cdcaaf
John <tim@cruz-family.ws>
2023-03-28 09:26:46 -0700

    tried to fix styles

--- react-app/src/components/ClassDecks/index.js
+++ react-app/src/components/ClassDecks/index.js
@@ -3,32 +3,13 @@ import { useRef } from "react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import CreateDeckModal from "../CreateDeckModal";
-import DeckObjectiveModal from "../DeckObjectiveModal";
-import DeleteDeckModal from "../DeleteDeckModal";
-import EditDeckModal from "../EditDeckModal";
 import OpenModalButton from "../OpenModalButton";
-import PreviewFlashcardsModal from "../PreviewFlashcardsModal";
+import SingleDeck from "../SingleDeck";
 import "./ClassDecks.css"
 
 const ClassDecks = ({ props }) => {
     const history = useHistory();
     const [session, chosenClass] = props;
-    const [openEditMenu, setOpenEditMenu] = useState(false);
-    const ulRef = useRef();
-
-    useEffect(() => {
-        if (!openEditMenu) return;
-
-        const closeMenu = (e) => {
-            if (!ulRef.current?.contains(e.target)) {
-                setOpenEditMenu(false);
-            }
-        };
-
-        document.addEventListener("click", closeMenu);
-
-        return () => document.removeEventListener("click", closeMenu);
-    }, [openEditMenu]);
 
     return (
         <div id="dashboard-decks-container">
@@ -45,81 +26,7 @@ const ClassDecks = ({ props }) => {
                 </div>
             </div>
             {chosenClass?.decks?.length ? chosenClass?.decks.map((deck, idx) => (
-                <div className="deck-container" onClick={() => history.push(`/study/decks/${deck?.id}`)} key={idx}>
-                    <div className="deck-container-left">
-                        <i className="fa-regular fa-circle fa-xl" />
-                        <div className="deck-progress-percent">%</div>
-                        <div className=" deck-info-container">
-                            <div className="deck-name">{deck.name}</div>
-                            <div className="deck-progress-bar"></div>
-                        </div>
-                    </div>
-                    <div className={`deck-container-right ${deck?.flashcards?.length ? "" : "empty"}`}>
-                        <div onClick={(e) => e.stopPropagation()} className={`deck-container-preview ${session?.user?.id === chosenClass?.user?.id ? deck?.flashcards?.length ? "" : "hidden" : "hidden"}`}>
-                            <OpenModalButton
-                                modalComponent={<PreviewFlashcardsModal />}
-                                buttonText={<i className="fa-solid fa-glasses fa-lg" />}
-                            />
-                        </div>
-                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-edit fa-solid fa-pencil fa-lg" />
-                        <i onClick={(e) => {
-                            e.stopPropagation()
-                            setOpenEditMenu(true)
-                        }} className="deck-container-right-icons deck-container-options fa-solid fa-ellipsis fa-lg" />
-                        <div className={`deck-pop-up-container ${openEditMenu ? "" : "hidden"}`}>
-                            {session?.user?.id === chosenClass?.user?.id ?
-                                <>
-                                    <div className="deck-pop-up-section" onClick={(e) => e.stopPropagation()}>
-                                        <OpenModalButton
-                                            buttonText={
-                                                <>
-                                                    <i className="fa-solid fa-circle-info" />
-                                                    <p>Deck Objective</p>
-                                                </>
-                                            }
-                                            modalComponent={<DeckObjectiveModal props={[deck]} />}
-                                        />
-                                    </div>
-                                    <div className="deck-pop-up-section" onClick={(e) => e.stopPropagation()}>
-                                        <OpenModalButton
-                                            buttonText={
-                                                <>
-                                                    <i class="fa-solid fa-pencil" />
-                                                    <p>Edit Deck Info</p>
-                                                </>
-                                            }
-                                            modalComponent={<EditDeckModal props={[chosenClass, deck]} />}
-                                        />
-                                    </div>
-                                    <div className="deck-pop-up-section" onClick={(e) => e.stopPropagation()}>
-                                        <OpenModalButton
-                                            buttonText={<>
-                                                <i className="fa-solid fa-xmark" />
-                                                <p>Remove this Deck</p>
-                                            </>}
-                                            modalComponent={<DeleteDeckModal props={[chosenClass, deck]} />}
-                                        />
-                                    </div>
-                                </>
-                                :
-                                <div onClick={(e) => e.stopPropagation()}>
-                                    <OpenModalButton
-                                        buttonText={
-                                            <>
-                                                <i className="fa-solid fa-circle-info" />
-                                                <p>Deck Objective</p>
-                                            </>
-                                        }
-                                        modalComponent={<DeckObjectiveModal />}
-                                    />
-                                </div>
-                            }
-                        </div>
-                        <div onClick={(e) => e.stopPropagation()} className="deck-container-add">ADD CARDS</div>
-                        <i className="deck-container-right-icons deck-container-study fa-solid fa-circle-play fa-lg" />
-                        <i onClick={(e) => e.stopPropagation()} className="deck-container-right-icons deck-container-view fa-solid fa-angle-right fa-lg" />
-                    </div>
-                </div>
+                <SingleDeck props={[deck, chosenClass, session]} key={idx} />
             ))
                 :
                 <div id="empty-deck-message-container">
