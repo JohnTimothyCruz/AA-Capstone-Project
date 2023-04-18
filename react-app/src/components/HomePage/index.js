22c941dca5b14e757b4a0171b8cca90334582427
John <tim@cruz-family.ws>
2023-04-17 17:25:21 -0700

    made homepage carousel

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -3,14 +3,60 @@ import { useHistory } from "react-router-dom";
 import SignupFormModal from "../SignupFormModal";
 import "./HomePage.css";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
     const history = useHistory();
 
     const session = useSelector(state => state.session)
 
+    const swapImage = () => {
+        const imageContainer = document.getElementById("images-container")
+        const imageList = document.getElementsByClassName("carousel-image")
+        const firstImage = imageList[0]
+
+        imageContainer.classList.add("transitioning")
+
+        imageContainer.addEventListener("animationend", () => {
+            imageContainer.removeChild(firstImage)
+            imageContainer.appendChild(firstImage)
+            imageContainer.classList.remove("transitioning")
+        })
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            swapImage()
+        }, 2500)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    const images = [
+        "https://www.brainscape.com/pks/static/assets/images/cms/public-views/pages/landing/band-1/image-1-e10d192395aee617c5e9.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-2.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-3.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-4.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-5.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-6.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-7.jpg",
+        "https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-8.jpg"
+    ]
+
     return (
         <div id="homepage">
+            <div id="image-carousel">
+                <div id="images-container">
+                    <img className="carousel-image" src="https://www.brainscape.com/pks/static/assets/images/cms/public-views/pages/landing/band-1/image-1-e10d192395aee617c5e9.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-2.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-3.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-4.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-5.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-6.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-7.jpg"></img>
+                    <img className="carousel-image" src="https://www.brainscape.com/assets/cms/public-views/pages/landing/band-1/image-8.jpg"></img>
+                </div>
+            </div>
             <div id="homepage-text-container">
                 <h2 className="homepage-h2">Rise to</h2>
                 <h2 className="homepage-h2"> your challenge.</h2>
@@ -21,14 +67,17 @@ const HomePage = () => {
                     <div
                         id="find-flashcards-button"
                         onClick={() => history.push("/classes")}
-                    >Find Flashcards</div>
+                        >Find Flashcards</div>
                     {!session?.user?.id &&
                         <OpenModalButton
-                            modalComponent={<SignupFormModal />}
-                            buttonText="Make Flashcards"
+                        modalComponent={<SignupFormModal />}
+                        buttonText="Make Flashcards"
                         />
                     }
                 </div>
+                {/* <div onClick={() => swapImage()}>
+                    test here
+                </div> */}
             </div>
         </div>
     )
